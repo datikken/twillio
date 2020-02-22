ecb729e757d6c38e8705fa3398275b423858393e
tikken <tikken23@gmail.com>
2020-02-22 13:08:44 +0300

    finished

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.onCreatePage = async ({ page, actions }) => {
+  if (page.path.match(/^\/room/)) {
+    page.matchPath = '/room/*';
 
-// You can delete this file if you're not using it
+    actions.createPage(page);
+  }
+};
